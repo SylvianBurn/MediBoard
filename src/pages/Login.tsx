e4bc775eda77a4ee115e2effc9c46c1423e97514
Sylvian BURN <sylvian.burn@epitech.eu>
2024-05-27 08:41:18 +0800

    feat: handle error messages display

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,45 +6,61 @@ import {
   Avatar,
   Typography,
   TextField,
-  Button,
-  Grid,
 } from "@mui/material";
-import axios from "axios";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import LoadingButton from '@mui/lab/LoadingButton';
+import { useNavigate } from "react-router-dom";
+import LoadingButton from "@mui/lab/LoadingButton";
 import { loginRequest } from "../utils/api";
-import { Role, useAuth } from '../utils/ProtectedRoute';
-import LoginIcon from '@mui/icons-material/Login';
+import { useAuth } from "../utils/ProtectedRoute";
+import LoginIcon from "@mui/icons-material/Login";
+
+const concatStrings = (tab: { message: string }[]) => {
+  var strings: string[] = [];
+
+  tab.map((value) => {
+    strings.push(value.message);
+  });
+  return strings;
+};
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const ax = axios.create();
   const [isLoginLoading, setIsLoginLoading] = useState(false);
-  const {authenticateUser} = useAuth();
-
-  // ax.defaults.headers.post["Access-Control-Allow-Origin"] =
-  //   "http://localhost:3336";
+  const { authenticateUser } = useAuth();
+  const [errorMsgs, setErrorMsgs] = useState<string[]>([]);
 
   const handleLoginRequest = async (email: string, password: string) => {
-    console.log("trying to login with data:", { email, password });
-    try {
-      setIsLoginLoading(true);
-      const res = await loginRequest(email, password);
-      setIsLoginLoading(false);
-      if (res.status === 200) {
-        console.log("res:", res);
-        if (res.data.token) {
-          console.log("token:", res.data.token.token);
-          authenticateUser(res.data.token.token, res.data.administrator);
-          navigate("/");
+    // console.log("trying to login with data:", { email, password });
+    setErrorMsgs([]);
+    setIsLoginLoading(true);
+    loginRequest(email, password)
+      .then((res) => {
+        if (res.status === 200) {
+          // console.log("res:", res);
+          if (res.data.token) {
+            // console.log("token:", res.data.token.token);
+            authenticateUser(res.data.token.token, res.data.administrator);
+            navigate("/");
+          }
         }
-      }
-    } catch (error) {
-      console.error("Error during login attempt:", error);
-    }
+      })
+      .catch((err) => {
+        if (
+          err.response.status === 400 &&
+          err.response.data.errors[0].message === "Invalid user credentials"
+        ) {
+          setErrorMsgs(concatStrings(err.response.data.errors));
+        } else if (err.response.status === 422) {
+          setErrorMsgs(concatStrings(err.response.data.errors));
+        } else {
+          console.error("an error occured during login attempt:", err);
+        }
+      })
+      .finally(() => {
+        setIsLoginLoading(false);
+      });
   };
 
   const handleLogin = () => {
@@ -79,7 +95,6 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-
             <TextField
               margin="normal"
               required
@@ -93,18 +108,34 @@ const Login = () => {
                 setPassword(e.target.value);
               }}
             />
-
             <LoadingButton
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
               onClick={handleLogin}
               loading={isLoginLoading}
-              loadingPosition='start'
+              loadingPosition="start"
               startIcon={<LoginIcon />}
             >
               Login
             </LoadingButton>
+            {errorMsgs && (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  rowGap: "6px",
+                }}
+              >
+                {errorMsgs.map((errorMsg) => {
+                  return (
+                    <Typography variant="body2" color="error">
+                      {errorMsg}
+                    </Typography>
+                  );
+                })}
+              </div>
+            )}
             {/* <Grid container justifyContent={"flex-end"}>
               <Grid item>
                 <Link to="/register">Don't have an account? Register</Link>
